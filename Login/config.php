fba7bec950b4722dde6332a4165396bd24f619da
António Cunha <antonio.cunh@ua.pt>
2019-11-22 20:46:25 +0000

    Conexao a base de dados

--- Login/config.php
+++ Login/config.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $servername = "estga-dev.clients.ua.pt";
+    $username = "ppsi-2019-gr5";
+    $password = "S%#5H6z8r+";
 
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
+</body>
+</html>
