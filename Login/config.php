5bb14ec36444f0c513b32ff8bf8d39d47a42d209
Toneca35 <antoniocunha505@gmail.com>
2019-11-25 23:46:50 +0000

    Compatibilidade com todos os tipos de caracteres

--- Login/config.php
+++ Login/config.php
@@ -5,7 +5,7 @@
     $password = "S%#5H6z8r+";
 
     try{
-    $conn = new PDO("mysql:host=$servername;dbname=ppsi-2019-gr5", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=ppsi-2019-gr5", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         echo $e->getMessage();
