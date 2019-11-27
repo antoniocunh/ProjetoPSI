c635a2f85d44618686271a5e6a45bf93eb69ab21
Toneca35 <antoniocunha505@gmail.com>
2019-11-27 17:02:08 +0000

    Validação de dados inseridos com mensagens de aviso

--- Login/registo.php
+++ Login/registo.php
@@ -1,15 +1,34 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="pt">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
+<?php 
+        session_start();
+        function fnValidateInput($name){
+            if (isset($_SESSION[$name])){
+                 if (!$_SESSION[$name][0]){ 
+                    echo ' is-invalid " value="' . $_SESSION[$name][1] . '"';
+                 }else{
+                    echo '" value="' . $_SESSION[$name][1] . '"';
+                 }
+            }
+        }
+
+        function fnValidateInputMessage($name, $message){
+            if (isset($_SESSION[$name])){
+                 if (!$_SESSION[$name][0]){ 
+                    echo '<small id="passwordHelp" class="text-danger">' . $message . '</small>';
+                 }
+            }
+        }
+    ?> 
     <section>
         <div class="container">
             <div class="row justify-content-center">
@@ -22,80 +41,91 @@
                         </div>
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="nome" type="text" class="form-control is-invalid" placeholder="Nome">
-                                <small id="passwordHelp" class="text-danger">
-                                    Must be 8-20 characters long.
-                                </small>     
+                                <input name="nome" type="text" placeholder="Nome" class="form-control <?php fnValidateInput('nome');?>">      
+                                <?php fnValidateInputMessage('nome', 'Must be 8-20 characters long.'); ?>                                                                                             
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="ultimoNome" type="text" class="form-control" placeholder="Sobrenome">
+                                <input name="ultimoNome" type="text" placeholder="Sobrenome" class="form-control <?php fnValidateInput('ultimoNome');?>">      
+                                <?php fnValidateInputMessage('ultimoNome', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="ambito" type="text" class="form-control" placeholder="Âmbito">
+                                <input name="ambito" type="text" placeholder="Âmbito" class="form-control <?php fnValidateInput('ambito');?>">      
+                                <?php fnValidateInputMessage('ambito', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="morada"type="text" class="form-control" placeholder="Morada">
+                                <input name="morada" type="text" placeholder="Morada" class="form-control <?php fnValidateInput('morada');?>">      
+                                <?php fnValidateInputMessage('morada', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="telefone" type="text" class="form-control" placeholder="Número de Telemóvel">
+                                <input name="telefone" type="text" placeholder="Número de Telemóvel" class="form-control <?php fnValidateInput('telefone');?>">      
+                                <?php fnValidateInputMessage('telefone', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="pais" type="text" class="form-control" placeholder=" País">
+                                <input name="pais" type="text" placeholder=" País" class="form-control <?php fnValidateInput('pais');?>">      
+                                <?php fnValidateInputMessage('pais', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="cidade" type="text" class="form-control" placeholder=" Cidade">
+                                <input name="cidade" type="text" placeholder=" Cidade" class="form-control <?php fnValidateInput('cidade');?>">      
+                                <?php fnValidateInputMessage('cidade', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="codPostal" type="text" class="form-control" placeholder=" Código Postal">
+                                <input name="codPostal" type="text" placeholder=" Código Postal" class="form-control <?php fnValidateInput('codpostal');?>">      
+                                <?php fnValidateInputMessage('codpostal', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="organizacao" type="text" class="form-control" placeholder=" Organização">
+                                <input name="organizacao" type="text" placeholder=" Organização" class="form-control <?php fnValidateInput('organizacao');?>">      
+                                <?php fnValidateInputMessage('organizacao', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
 
                         <div class="row align-items-center mt-4">
                             <div class="col">
-                                <input name="email" type="email" class="form-control" placeholder="Email">
+                                <input name="email" type="email" placeholder="Email" class="form-control <?php fnValidateInput('email');?>">      
+                                <?php fnValidateInputMessage('email', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center">
                             <div class="col mt-4">
-                                <input name="username" type="text" class="form-control" placeholder=" Username">
+                                <input name="username" type="text" placeholder=" Username" class="form-control <?php fnValidateInput('username');?>">      
+                                <?php fnValidateInputMessage('username', 'Must be 8-20 characters long.'); ?>    
                             </div>
                         </div>
 
                         <div class="row align-items-center mt-4">
                             <div class="col">
-                                <input name="pass1" type="password" class="form-control" placeholder="Password">
+                                <input name="pass1" type="password" placeholder="Password" class="form-control <?php fnValidateInput('pass1');?>">      
+                                <?php fnValidateInputMessage('pass1', 'Must be 8-20 characters long.'); ?>    
                             </div>
                             <div class="col">
-                                <input name="pass2" type="password" class="form-control" placeholder="Confirmar Password">
+                                <input name="pass2" type="password" placeholder="Confirmar Password" class="form-control <?php fnValidateInput('pass2'); ?>">      
+                                <?php fnValidateInputMessage('pass2', 'Must be 8-20 characters long.'); 
+                                session_destroy();?>    
                             </div>
                         </div>
                         <div class="row justify-content-start mt-4">
