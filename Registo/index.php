cdf748b2b04ddf53715590c4a58aa8b5d0358659
Toneca35 <antoniocunha505@gmail.com>
2019-11-28 13:08:01 +0000

    atualizou para testes

--- Registo/index.php
+++ Registo/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="../asset/css/paper-kit.css?v=2.2.0" rel="stylesheet" />
+    <link href="../asset/css/paper-kit.css?v=2.2.0" rel="stylesheet"/>
 </head>
 
 <body>
