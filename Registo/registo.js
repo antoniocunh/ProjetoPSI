d7b8ab8ce969b38ac411fe8822cea20f6c57b825
Toneca35 <antoniocunha505@gmail.com>
2019-12-02 18:07:49 +0000

    adicionou novos tipos de validação

--- Registo/registo.js
+++ Registo/registo.js
@@ -3,17 +3,31 @@ $(function () {
             url: "../assets/php/apis/getAmbito.php",
             success: function (result) {
                 teste = JSON.parse(result);
-                
-                console.log(result);
                 teste.forEach(function (item) {
-                    console.log(item);
                     $('#scope').append("<option value='" + item.iIdScope + "'> "+ item.vcName + "</option>");
                 });
             }
         }),
 
+        $.validator.addMethod("maior18", function(value, element) {
+            chooseDate = new Date(value);
+            atualDate = new Date(Date.now());
+            if(atualDate.getFullYear() - chooseDate.getFullYear() == 18){
+                if(chooseDate.getMonth() <= atualDate.getMonth()){      
+                    if(chooseDate.getDate() <= atualDate.getDate()){      
+                        return true;
+                    }
+                }
+            }
+            if(atualDate.getFullYear() - chooseDate.getFullYear() > 18){
+                return true;
+            }
+            return false;
+        });
+
 
         $('form[name="registo"]').validate({
+            debug: true,
             errorElement: "div",
             rules: {
                 nome: 'required',
@@ -23,21 +37,42 @@ $(function () {
                 telefone: 'required',
                 pais: 'required',
                 cidade: 'required',
-                dataNascimento: 'required',
+                dataNascimento: {
+                    required: true,
+                    maior18: true,
+                    date: true
+                },
                 email: {
                     required: true,
                     email: true,
+                    remote: {
+                        url: "../assets/php/apis/getEmail.php",
+                        type: "post",
+                        data: {
+                          username: function() {
+                            return $( "#email" ).val();
+                          }
+                        }
+                      }
                 },
                 username: {
                     required: true,
-                    minlength: 6,
+                    minlength: 4,
+                    remote: {
+                        url: "../assets/php/apis/getUsername.php",
+                        type: "post",
+                        data: {
+                          username: function() {
+                            return $( "#username" ).val();
+                          }
+                        }
+                      }
                 },
                 pass1: {
                     required: true,
                     minlength: 8,
                 },
                 pass2: {
-                    required: true,
                     equalTo: "#pass1"
                 },
             },
@@ -49,14 +84,17 @@ $(function () {
                 telefone: 'Por favor introduza um nº de telefone.',
                 pais: 'Por favor selecione um pais.',
                 cidade: 'Por favor introduza uma cidade.',
-                dataNascimento:'Por favor introduza a sua data de Nascimento',
+                dataNascimento:{
+                    required: 'Por favor introduza a sua data de Nascimento',
+                    maior18: 'Tem de ser maior de 18 para poder se registar.'
+                },
                 email: {
                     required: 'Por favor introduza um email.',
                     email: 'Por favor introduza um email válido.',
                 },
                 username: {
                     required: 'Por favor introduza um username.',
-                    minlength: 'O user tem de ter pelo menos 6 caracteres.'
+                    minlength: 'O username tem de ter pelo menos 4 caracteres.'
                 },
                 pass1: {
                     required: 'Por favor introduza uma palavra-passe.',
@@ -67,7 +105,7 @@ $(function () {
                     equalTo: 'A palavra-passe não é igual á introduzida anteriormente'
                 },
             },
-            submitHandler: function (form) {
+            submitHandler: function(form){
                 form.submit();
             }
         });
