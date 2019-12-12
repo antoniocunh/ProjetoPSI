215ffc37e7103c47f818a6b0036094ea7aaa8c3d
António Cunha <antonio.cunh@ua.pt>
2019-12-12 23:02:31 +0000

    correção final

--- login/script/login.js
+++ login/script/login.js
@@ -32,12 +32,12 @@ $('document').ready(function() {
 			success : function(response){
 				teste = JSON.parse(response);	
 				console.log(response);
-				if(teste.one =='ok'){										
+				if(teste.msg =='ok'){										
 					$("#login_button").html('<img src="ajax-loader.gif" /> &nbsp; Signing In ...');
 					setTimeout(' window.location.href = "../Users/home-user-page.html"; ',0);
 				} else {									
 					$("#error").fadeIn(1000, function(){						
-						$("#error").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+teste.one+' !</div>');
+						$("#error").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; '+teste.msg+' !</div>');
 						$("#login_button").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In');
 					});
 				}
