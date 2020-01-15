67b1d5c92195b37fcb1b04360b6561d4534dd64c
Toneca35 <antoniocunha505@gmail.com>
2020-01-15 18:11:33 +0000

    retoques no design

--- Login/includes/reset-password.php
+++ Login/includes/reset-password.php
@@ -83,7 +83,6 @@ require_once 'obj.ResetPassword.php';
     </div>
   </div>
   <!--   Core JS Files   -->
-  <script src="../../assets/js/core/jquery.min.js" type="text/javascript"></script>
   <script src="../../assets/js/core/popper.min.js" type="text/javascript"></script>
   <script src="../../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
   <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
